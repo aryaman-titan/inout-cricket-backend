f61f53d75ef149f22b7ac3569d20b76c7bfcb73a
aryaman-titan <aryamanbehera@gmail.com>
2020-12-12 00:06:32 +0530

    websocket server node

---
+++ server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+import express from 'express';
+import { Server } from 'ws';
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const four = {
+  name_of_shot: '4',
+  batsman: 'Aryaman',
+  bowler: 'Hellix',
+  umpire: 'Aviral'
+}
+
+const six = {
+  name_of_shot: '6',
+  batsman: 'Aryaman',
+  bowler: 'Hellix',
+  umpire: 'Aviral'
+}
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  ws.on('message', function incoming(data) {
+    console.log(data);
+    console.log(wss.clients);
+    
+    switch (data) {
+      case '4':
+        wss.clients.forEach((client) => {
+          if (client !== ws ) {
+            client.send(JSON.stringify(four));
+          }
+        })
+        break;
+      case '6':
+        wss.clients.forEach((client) => {
+          if (client !== ws ) {
+            client.send(JSON.stringify(six));
+          }
+        })
+        break;
+      default:
+        client.send('No info!');
+        break;
+    }
+  });
+
+  ws.on('close', () => console.log('Client disconnected'));
+});
